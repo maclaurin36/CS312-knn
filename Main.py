4fb3427bd46f7517a4a73a9489814b25a556406e
JesseClark36 <jesse.clark@awardco.com>
2022-11-04 18:35:40 -0600

    Finished a bit more, just the features left

--- Main.py
+++ Main.py
@@ -8,6 +8,8 @@ from scipy.io import arff
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import heapq
 
@@ -57,34 +59,34 @@ class KNNClassifier(BaseEstimator, ClassifierMixin):
         k_dictionary = {}
         for row in X:
             new_x, distances, new_y = get_distances(self.X, self.y, row)
-            for i, k in enumerate(k_array):
-                new_x = get_top_k(new_x, k)
-                new_y = get_top_k(new_y, k)
-                distances = get_top_k(distances, k)
+            for k in k_array:
+                new_x_copy = get_top_k(new_x, k)
+                new_y_copy = get_top_k(new_y, k)
+                distances_copy = get_top_k(distances, k)
                 if self.regression:
                     if self.weight_type == 'inverse_distance':
-                        inv_distances = get_inv_dist_squared(distances)
-                        numerator = dot_col_vecs(inv_distances, new_y)
+                        inv_distances = get_inv_dist_squared(distances_copy)
+                        numerator = dot_col_vecs(inv_distances, new_y_copy)
                         denominator = inv_distances.sum()
                         guess = float(numerator)/denominator
                         self.add_k_guess(k_dictionary, guess, k)
                     else:
-                        guess = new_y.sum()/float(get_num_elements(new_y))
+                        guess = new_y_copy.sum()/float(get_num_elements(new_y_copy))
                         self.add_k_guess(k_dictionary, guess, k)
                 else:
                     if self.weight_type == 'inverse_distance':
-                        inv_distances = get_inv_dist_squared(distances)
-                        aggregates = get_sum_aggregates(np.append(inv_distances[..., None], new_y[..., None], axis=1), 1, 0)
+                        inv_distances = get_inv_dist_squared(distances_copy)
+                        aggregates = get_sum_aggregates(np.append(inv_distances[..., None], new_y_copy[..., None], axis=1), 1, 0)
                         guess = aggregates[0, 0]
                         self.add_k_guess(k_dictionary, guess, k)
                     else:
-                        guess = get_mode(new_y)
+                        guess = get_mode(new_y_copy)
                         self.add_k_guess(k_dictionary, guess, k)
 
         return k_dictionary
 
     # Returns the Mean score given input data and labels
-    def score(self, X, y, k):
+    def score(self, X, y, k_array):
         """ Return accuracy of model on a given dataset. Must implement own score function.
         Args:
             X (array-like): A 2D numpy array with data, excluding targets
@@ -93,13 +95,20 @@ class KNNClassifier(BaseEstimator, ClassifierMixin):
             score : float
                 Mean accuracy of self.predict(X) wrt. y.
         """
-        k_dictionary = self.predict(X, [k])
-        results = k_dictionary[k]
-        correct = 0
-        for i in range(0, len(results)):
-            if results[i] == y[i]:
-                correct += 1
-        return correct / len(results)
+        k_dictionary = self.predict(X, k_array)
+        finalScores = []
+        for k in k_array:
+            results = k_dictionary[k]
+            if self.regression:
+                mse = ((y - results)**2).sum() / float(len(y))
+                finalScores.append(mse)
+            else:
+                correct = 0
+                for i in range(0, len(results)):
+                    if results[i] == y[i]:
+                        correct += 1
+                finalScores.append(float(correct) / len(results))
+        return finalScores
 
     def get_prediction_by_count(self, y):
         return get_mode(y)
@@ -115,7 +124,6 @@ def real_dist(X, newPoint, ax=1):
     s = (x.conj() * x).real
     sqrtFunction = np.vectorize(sqrt)
     return sqrtFunction(add.reduce(s, axis=ax, keepdims=False))
-    # return np.linalg.norm(X - newPoint, axis=ax)
 
 def sqrt(value):
     return math.sqrt(value)
@@ -126,7 +134,7 @@ def cat_dist(X, newPoint, ax=1):
     return np.linalg.norm(X != newPoint, axis=ax)
 
 def get_inv_dist_squared(distances):
-    return 1.0 / (distances**2)
+    return 1.0 / (distances**2+0.0000001)
 
 # Returns the X and y values sorted by distance from the new point along with their corresponding distances
 def get_distances(X, y, newPoint):
@@ -174,7 +182,9 @@ def get_num_elements(array):
     return product
 
 def decodeBytes(value):
-    return value.decode()
+    if type(value) == 'bytes':
+        return value.decode()
+    return value
 
 
 def do_debug():
@@ -190,11 +200,11 @@ def do_debug():
 
     knn = KNNClassifier(weight_type="")
     knn.fit(train_x, train_y)
-    print(knn.score(test_x, test_y, 3))
+    print(knn.score(test_x, test_y, [3]))
 
     knn2 = KNNClassifier()
     knn2.fit(train_x, train_y)
-    print(knn2.score(test_x, test_y, 3))
+    print(knn2.score(test_x, test_y, [3]))
 
 def do_eval():
     vectorizedDecoder = np.vectorize(decodeBytes)
@@ -209,11 +219,11 @@ def do_eval():
 
     knn = KNNClassifier(weight_type="")
     knn.fit(train_x, train_y)
-    print(knn.score(test_x, test_y, 3))
+    print(knn.score(test_x, test_y, [3]))
 
     knn2 = KNNClassifier()
     knn2.fit(train_x, train_y)
-    print(knn2.score(test_x, test_y, 3))
+    print(knn2.score(test_x, test_y, [3]))
 
 def do_magic():
     vectorizedDecoder = np.vectorize(decodeBytes)
@@ -228,7 +238,122 @@ def do_magic():
 
     knn = KNNClassifier(weight_type="")
     knn.fit(train_x, train_y)
-    print(knn.score(test_x, test_y, 3))
+    # print(knn.score(test_x, test_y, [1,3,5,7,9,11,13,15]))
+
+    combined_data = np.concatenate((train_x,test_x))
+    combined_normalized = normalize_data(combined_data)
+    num_train_x_rows, _ = train_x.shape
+
+    normalized_train_x = combined_normalized[:num_train_x_rows, :]
+    normalized_test_x = combined_normalized[num_train_x_rows:, :]
+
+    knn2 = KNNClassifier(weight_type="")
+    knn2.fit(normalized_train_x, train_y)
+    print(knn2.score(normalized_test_x, test_y, [1,3,5,7,9,11,13,15]))
+
+
+def normalize_data(array):
+    newarray = array.copy()
+    _, num_cols = array.shape
+    for i in range(0, num_cols):
+        curCol = array[:,i]
+        curColMax = np.max(curCol)
+        curColMin = np.min(curCol)
+        def normalize(value):
+            return (value - curColMin) / float(curColMax - curColMin)
+        normalizeVectorized = np.vectorize(normalize)
+        newarray[:,i] = normalizeVectorized(curCol)
+    return newarray
+
+def do_housing():
+    vectorizedDecoder = np.vectorize(decodeBytes)
+    training_data = vectorizedDecoder(np.array(load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/housing_train.arff")))
+    train_x = training_data[:,:-1]
+    train_y = training_data[:,-1]
+
+    testing_data = vectorizedDecoder(np.array(load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/housing_test.arff")))
+    test_x = testing_data[:,:-1]
+    test_y = testing_data[:,-1]
+
+    combined_data = np.concatenate((train_x, test_x))
+    combined_normalized = normalize_data(combined_data)
+    num_train_x_rows, _ = train_x.shape
+
+    normalized_train_x = combined_normalized[:num_train_x_rows, :]
+    normalized_test_x = combined_normalized[num_train_x_rows:, :]
+
+    knn = KNNClassifier(weight_type='',regression=True)
+    knn.fit(normalized_train_x, train_y)
+    k_array = [1,3,5,7,9,11,13,15]
+    mses = knn.score(normalized_test_x, test_y, k_array)
+
+    plt.plot(k_array, mses)
+    plt.title("Housing MSE by k-value")
+    plt.xlabel("K-value")
+    plt.ylabel("MSE")
+    plt.show()
+
+
+def do_magic_distance_weighting():
+    vectorizedDecoder = np.vectorize(decodeBytes)
+
+    training_data = np.array(
+        load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/magic_telescope_train.arff"))
+    train_x = training_data[:, :-1]
+    train_y = vectorizedDecoder(training_data[:, -1])
+
+    testing_data = np.array(
+        load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/magic_telescope_test.arff"))
+    test_x = testing_data[:, :-1]
+    test_y = vectorizedDecoder(testing_data[:, -1])
+
+    combined_data = np.concatenate((train_x, test_x))
+    combined_normalized = normalize_data(combined_data)
+    num_train_x_rows, _ = train_x.shape
+
+    normalized_train_x = combined_normalized[:num_train_x_rows, :]
+    normalized_test_x = combined_normalized[num_train_x_rows:, :]
+
+    knn = KNNClassifier()
+    knn.fit(normalized_train_x, train_y)
+    print(knn.score(normalized_test_x, test_y, [3]))
+
+
+def do_housing_distance_weighting():
+    vectorizedDecoder = np.vectorize(decodeBytes)
+    training_data = vectorizedDecoder(np.array(load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/housing_train.arff")))
+    train_x = training_data[:,:-1]
+    train_y = training_data[:,-1]
+
+    testing_data = vectorizedDecoder(np.array(load_data(r"https://raw.githubusercontent.com/cs472ta/CS472/master/datasets/housing_test.arff")))
+    test_x = testing_data[:,:-1]
+    test_y = testing_data[:,-1]
+
+    combined_data = np.concatenate((train_x, test_x))
+    combined_normalized = normalize_data(combined_data)
+    num_train_x_rows, _ = train_x.shape
+
+    normalized_train_x = combined_normalized[:num_train_x_rows, :]
+    normalized_test_x = combined_normalized[num_train_x_rows:, :]
+
+    knn = KNNClassifier(regression=True)
+    knn.fit(normalized_train_x, train_y)
+    k_array = [1,3,5,7,9,11,13,15]
+    mses = knn.score(normalized_test_x, test_y, k_array)
+
+    plt.plot(k_array, mses)
+    plt.title("Housing MSE by k-value")
+    plt.xlabel("K-value")
+    plt.ylabel("MSE")
+    plt.show()
+
+
+def do_credit():
 
 if __name__ == "__main__":
-    do_magic()
+    # do_debug()
+    # do_eval()
+    # do_magic()
+    # do_housing()
+    # do_magic_distance_weighting()
+    do_housing_distance_weighting()
